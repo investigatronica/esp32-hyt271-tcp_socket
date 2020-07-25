e8f553be2e66eb7d6f500b03c94ed852c872071b
investigatronica <gax@openmailbox.org>
2020-07-25 11:47:05 -0300

    microide

--- boot.py
+++ boot.py
@@ -1,4 +1,56 @@
-import os
-import pycom
-pycom.wifi_on_boot(False)
-pycom.heartbeat(False)
+# microIDE Filename: /boot.py
+### This file is executed on every boot (including wake-boot from deepsleep)
+import _thread
+
+### NOOB WARNING: DO NOT ADD ANYTHING ELSE HERE !!!
+### If boot.py has an error it will break and you are locked out
+### Use main.py to run your code.
+
+
+### Connect to existing Network - input SSID & PASSWORD
+def wifi():
+  import network
+  from time import sleep
+  sta_if = network.WLAN(network.STA_IF)
+  while True:
+    sta_if.active(True)
+    sta_if.connect("maxmax3", "s0l0mi02s0l0mi0")
+    sleep(5)
+    if sta_if.isconnected():
+      sleep(300)
+### Uncomment to enable
+_thread.start_new_thread(wifi,())
+
+### Enable the AP with SSID "microIDE" (IP: 192.168.4.1)
+#import network
+#ap = network.WLAN(network.AP_IF)
+#ap.active(True)
+#ap.config(essid="microIDE")
+
+
+### Creates a Reset - it will reset the chip every 10 minutes
+def rst():
+  from time import sleep
+  sleep(590)
+  print("Reset imminent")
+  sleep(10)
+  import machine
+  machine.reset()
+#_thread.start_new_thread(rst,())
+
+### Creates a WatchDog and a feeding loop - if _thread hangs it will reset.
+def wdt():
+  from machine import WDT
+  from time import sleep
+  wdt=WDT(timeout=10000) #10 seconds of hanging
+  while True:
+    wdt.feed()
+    sleep(1)
+_thread.start_new_thread(wdt,())
+
+
+### This enables the webserver and adds some functions for main.py
+from microIDE import wget , epoch ,millis
+# wget(filename)    # get a files from https://git.microIDE.com
+# epoch()           # UNIX epoch (seconds) - Use for long-term data
+# millis()          # Miliseconds since Boot - Use for short-term data
--- main.py
+++ main.py
@@ -1,106 +1,62 @@
-from network import LoRa
-import socket
+# microIDE Filename: /main.py
+### This file is executed after boot.py and runs after microIDE
+###
+### Uncomment the followong resource files  - they are static and won't overwrite
+### By default .gz files won't show in the directory
+### if these comments turn green the files were implemented correctly
+
+### Modes: Change the color of the editor based on extention
+wget('mode-python.js.gz')
+#wget('mode-css.js.gz')
+#wget('mode-html.js.gz')
+#wget('mode-javascript.js.gz')
+
+
+### Workers: Auto-close brackets, error-check, etc.
+#wget('worker-css.js.gz')
+#wget('worker-html.js.gz')
+#wget('worker-javascript.js.gz')
+
+### More at https://git.microIDE.com
 import time
-import ubinascii
-import struct
-from machine import Pin
+from machine import Pin, I2C
+#import network
+import socket
 
-# Initialise LoRa in LORAWAN mode.
-# Please pick the region that matches where you are using the device:
-# Asia = LoRa.AS923
-# Australia = LoRa.AU915
-# Europe = LoRa.EU868
-# United States = LoRa.US915
-#lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.US915)
-lora = LoRa(mode=LoRa.LORAWAN)
-lora.nvram_restore()
-
-for i in range(8, 72):
-    lora.remove_channel(i)
-
-start = 903900000
-f_inc = 200000
-curr  = start
-
-for i in range(8):
-    print(curr)
-    lora.add_channel(index=i, frequency=curr, dr_min=0, dr_max=4)
-    curr += f_inc
-
-# # create an ABP authentication params
-dev_addr = struct.unpack(">l", ubinascii.unhexlify('2601117D'))[0]
-nwk_swkey = ubinascii.unhexlify('0547672E917858608AE0E407267E1368')
-app_swkey = ubinascii.unhexlify('860EFE85C4860C9C373550CF69C3D7C9')
-#
-# # join a network using ABP (Activation By Personalization)
-if not lora.has_joined():
-    lora.join(activation=LoRa.ABP, auth=(dev_addr, nwk_swkey, app_swkey))
+led = Pin(2, Pin.OUT)
+def pisca(tiempo):
+  led.on()
+  time.sleep(tiempo)
+  led.off()
+  time.sleep(tiempo)
+  led.on()
+  time.sleep(tiempo)
+  led.off()
+  time.sleep(tiempo)
 
 
+addr = socket.getaddrinfo('0.0.0.0', 81)[0][-1]
+s = socket.socket()
+s.bind(addr)
+s.listen(1)
+print('listening on', addr)
 
-# create an OTAA authentication parameters
-# milton
-#app_eui = ubinascii.unhexlify('70B3D57ED001319C')
-#app_key = ubinascii.unhexlify('E4563EC2FDEFD153F62ABD097070E2AE')
-# otaa
-#gax
-#app_eui = ubinascii.unhexlify('70B3D57ED0024DB4')
-#app_key = ubinascii.unhexlify('A60A4B17D6AD9DFEE262964BD8ADEF52')
-
-# join a network using OTAA (Over the Air Activation)
-#if not lora.has_joined():
-#    lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=0)
-
-# wait until the module has joined the network
-while not lora.has_joined():
-    time.sleep(0.5)
-    print('Not yet joined...')
-    pycom.rgbled(0xff0000)
-    time.sleep(0.5)
-    pycom.rgbled(0x000000)
-
-if lora.has_joined():
-    print("CONNECTED!!")
-    pycom.rgbled(0x00ff00)
-# create a LoRa socket
-s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
-
-# set the LoRaWAN data rate
-s.setsockopt(socket.SOL_LORA, socket.SO_DR, 0)
-# s.setsockopt(socket.SOL_LORA)
-
-# make the socket blocking
-# (waits for the data to be sent and for the 2 receive windows to expire)
-#s.setblocking(True)
-
-# send some data
-# while lora.has_joined():
-
+i2c = I2C(0)
+i2c = I2C(1, scl=Pin(5), sda=Pin(4), freq=400000)
+print("hola")
+print(i2c.scan())
 
 while True:
-
-    s.setblocking(True)
-    s.send("asd")
-    print("enviando")
-    pycom.rgbled(0x00ff00)
-    time.sleep(0.2)
-    pycom.rgbled(0x000000)
-    time.sleep(0.2)
-    pycom.rgbled(0x00ff00)
-    time.sleep(0.2)
-    pycom.rgbled(0x000000)
-    time.sleep(0.2)
-    pycom.rgbled(0x00ff00)
-# make the socket non-blocking
-#(because if there's no data received it will block forever...)
-    s.setblocking(False)
-
-# get any data received (if any...)
-    data = s.recv(64)
-    time.sleep(2)
-    lora.nvram_save()
-    if data:##print(data)
-        pycom.rgbled(0x0000ff)
-        time.sleep(2)
-        print(data)
-    time.sleep(57)
+  cl, addr = s.accept()
+  if cl:
+    i2c.writeto(40, b'123')
+    reading=i2c.readfrom(40, 4)
+    humedad = ((reading[0] & 0x3F) * 0x100 + reading[1]) * (100.0 / 16383.0)
+    temperatura = 165.0 / 16383.0 * ((reading[2] * 0x100 + (reading[3] & 0xFC)) >> 2) - 40
+    print('client connected from', addr)
+    print(str(cl.recv(100), 'utf8'))
+    print('{:5.3f}'.format(temperatura), '{:5.3f}'.format(humedad))
+    cl.send('{:5.3f}'.format(temperatura) +';'+ '{:5.3f}'.format(humedad))
+    cl.close()
+    pisca(.25)
+  time.sleep(.1)
